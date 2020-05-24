a89dea6d768e4b3f6525f1e84a22b24c6d6b5a0d
Denis Berezin <nocab139@yandex.ru>
2020-05-24 22:21:04 +0300

    Стилизация полей

--- js/main.js
+++ js/main.js
@@ -3,8 +3,25 @@ function testMy() {
     my = document.getElementById("my");
     my.innerHTML =line+ "%";
 }
+let inputs = document.querySelectorAll('.input__file');
+Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+        labelVal = label.querySelector('.input__file-button-text').innerText;
+
+    input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+            countFiles = this.files.length;
+
+        if (countFiles)
+            label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+        else
+            label.querySelector('.input__file-button-text').innerText = labelVal;
+    });
+});
 $(document).ready(function () {
     $('.js-order-select').on('click', function () {
         $(this).addClass('select_style');
     });
+
 });
